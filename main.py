fa9846e1667bbc0b063a0352c2b119b39375d5a3
leotian <leotiann@gmail.com>
2017-12-04 14:52:44 +0800

    refactor:

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ from wxbot import send_message
 def main():
     try:
         # 给用户名为小李的用户发送消息
-        send_message('hello world', '小李')
+        send_message('Hello World!', '小李')
     except Exception as e:
         logging.error('# 任务运行出错: ' + repr(e))
         traceback.print_exc()
