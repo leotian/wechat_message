d4b5dfccb6c42e6ccc5afe8639115404949eda57
leotian <leotiann@gmail.com>
2017-12-04 14:51:47 +0800

    docs:

--- README.md
+++ README.md
@@ -1,3 +1,9 @@
 # wechat_message
 
-提供一个方法，通过微信给
+提供一个方法，通过微信给 xx 发消息
+具体使用方法见main
+首次登陆可能需要扫码
+
+send_message(message, username)
+message: 消息内容
+username: 发送对象
--- main.py
+++ main.py
@@ -1,10 +1,12 @@
 import logging
 import traceback
 
+# 导入发消息方法
 from wxbot import send_message
 
 def main():
     try:
+        # 给用户名为小李的用户发送消息
         send_message('hello world', '小李')
     except Exception as e:
         logging.error('# 任务运行出错: ' + repr(e))
